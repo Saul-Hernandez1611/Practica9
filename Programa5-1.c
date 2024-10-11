b8b67dae36dc2e74fd8c650a818efc84a34e37bc
Saul-Hernandez1611 <saul56delizbeth@gmail.com>
2024-10-11 13:39:43 -0600

    Programa5-1.c

---
+++ Programa5-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+  char *ap, c = 'a'; 
+  ap = &c;
+  printf("Carácter: %c\n",*ap); 
+  printf("Código ASCII: %d\n",*ap); /
+  printf("Dirección de memoria: %d\n",ap);
+  return 0;
+}
