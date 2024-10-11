dd84695e27e79e3e1f2ad7b3d70b9851e7162281
Saul-Hernandez1611 <saul56delizbeth@gmail.com>
2024-10-11 13:20:41 -0600

    Programa1.c

---
+++ Programa1.c
@@ -0,0 +1,19 @@
+//FUNDAMENTOS DE LA PROGRAMACIÓN
+//ERICK SAUL GARCIA HERNANDEZ
+//PRACTICA 9
+//EJERCICIO 1
+
+#include <stdio.h>
+int main ()
+{
+  int Lista[5]  = {10, 8, 5, 8, 7};
+  int indice = 0;
+  printf("\tLista\n");
+  while (indice < 5)
+  {
+    printf("\nLa Calificacion del alumno es %d es %d, indice+1, Lista[indice]);
+    indice += 1;
+  }
+printf("\n");
+return 0;
+}
